3ac9f216a5938338e933f266677213b3d3204499
Peter Lobo <peter.lobo@mjv.com.br>
2019-11-15 13:27:13 -0500

    update front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit, Inject} from '@angular/core';
+import * as $ from 'jquery';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'a';
+export class AppComponent implements OnInit{
+
+  constructor(
+    @Inject(DOCUMENT) private document
+  ) {
+  }
+
+    ngOnInit(): void {
+
+    }
+    title = 'a';
+
+
 }
