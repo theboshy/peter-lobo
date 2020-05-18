24d59380c16502ad33d82dd2937d4431e47be384
Peter Lobo <peter.lobo@mjv.com.br>
2020-05-17 21:11:30 -0500

    Updates

---
+++ precache-manifest.463dce2dc6f67e3f2f0efe89acd2d61a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac77b4a2761362578f0c0d1226f2c16",
+    "url": "/peter-lobo/index.html"
+  },
+  {
+    "revision": "413818f70dfa7c442d16",
+    "url": "/peter-lobo/static/css/main.513ff9db.chunk.css"
+  },
+  {
+    "revision": "99b6104b230060140f86",
+    "url": "/peter-lobo/static/js/2.64d6dd2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/peter-lobo/static/js/2.64d6dd2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413818f70dfa7c442d16",
+    "url": "/peter-lobo/static/js/main.9f0254bb.chunk.js"
+  },
+  {
+    "revision": "d99128100ddba54fef8e",
+    "url": "/peter-lobo/static/js/runtime-main.8fa34b46.js"
+  }
+]);
